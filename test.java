38a35c3a0995e963a799cc776bc9e89c94b59b6f
hemsl <hemsl@DESKTOP-IQ1C21J>
2023-02-12 14:01:32 +0530

    changes

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i =1 ;
-		int j = i + 1;
+		int j = i + 2;
 		System.out.println("Arithmetic output : "+ j);
 	}
 
