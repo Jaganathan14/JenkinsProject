4acfa4480446c87ae42df3d874af08a272f69d43
hemsl <hemsl@DESKTOP-IQ1C21J>
2023-02-12 13:08:14 +0530

    test

---
+++ test.java
@@ -0,0 +1,11 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =1 ;
+		int j = i + 1;
+		System.out.println("Arithmetic output : "+ j);
+	}
+
+}
